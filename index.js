af0748200ed3dbe5c8fa4e1588ff4d0979bd2eb9
dylandiamond <dylandiamond34@gmail.com>
2022-07-31 19:30:01 -0400

    commit tree 6e6fc2dde839b5a2b2504373fedeac4c0c2f9d56

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(b, a) {
+    return b - a;
+
+}
+function multiply(a, b) {
+    return a * b;
+
+}
+function divide(a, b) {
+    return a / b;
+
+}
+function increment(number) {
+    return (number += 1)
+}
+function decrement(number) { 
+    return (number -= 1)
+}
+ function makeInt(number) {
+    return parseInt(number, 10)
+ }
+ function preserveDecimal(toString){
+    return parseFloat(toString)
+ }
